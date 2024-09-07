b57da3c05bdaa76f586942b09aadda5381d66e6d
Adrian Ditmansen <adriandenutrolige@gmail.com>
2024-09-07 12:18:15 +0200

    Alt er nå klart! Game on!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pyautogui
 import time
+import random
 
 
 #søker etter bilde og klikker
@@ -8,14 +9,44 @@ def finnBilde(navn):
     pyautogui.click(x,y)
 
 
+#prøver å klikke over troppene for å plassere
+def plyndring(navn):
+    finnBilde(navn)
+    x,y = pyautogui.position()
+    pyautogui.click(x,y-50,clicks=6,interval=random.randrange(0,5,1))
+
+
+#ser etter EndBattle så tar deg tilbake til landsby
+def EndComabat():
+    try:
+        pyautogui.locateOnScreen("Surrender.png",confidence=0.8)
+        time.sleep(7)
+        EndComabat()
+    except:
+        finnBilde("EndBattle.png")
+        time.sleep(1)
+        finnBilde("Okey.png")
+        time.sleep(1)
+        finnBilde("Home.png")
+
 
 
 #starter runde     
 finnBilde("attack.png")
 time.sleep(2)
 finnBilde("Find now.png")
-time.sleep(3)
-pyautogui.press("1")
+time.sleep(5)
+
+
+#combat
+plyndring("drage1.png")
+
+#avslutter combat
+EndComabat()
+
+
+
+
 
 
 
--- test.py
+++ test.py
@@ -1,3 +1,21 @@
-import pyautogui as auto
+import pyautogui
 
-auto.press("1")
+
+while True:
+    try:
+
+        gaming = pyautogui.locateOnScreen("attack.png", confidence=0.7)
+        print("online")
+
+    
+
+    except:
+        print("offline")
+
+
+#if gaming != pyautogui.ImageNotFoundException:
+#        print("Nei")
+#else:
+#        print("ja")
+
+
